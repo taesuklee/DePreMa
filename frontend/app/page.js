34599b4b2bd3b40699db4f3648c7307eb525a2fa
taesuklee <taesuk.lee@justtrade.com>
2024-02-07 21:12:18 +0100

    populate cards

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -4,16 +4,26 @@ import Card from '@/components/Card'
 import Header from '@/components/Header'
 
 export default function Home() {
+  const lists = [
+    { id: 1, team1: 'TEAM1', score1: 1, team2: 'TEAM2', score2: 7 },
+    { id: 1, team1: 'TEAM3', score1: 3, team2: 'TEAM7', score2: 3 },
+    { id: 1, team1: 'TEAM5', score1: 5, team2: 'TEAM4', score2: 4 },
+    { id: 1, team1: 'TEAM6', score1: 7, team2: 'TEAM3', score2: 3 },
+    { id: 1, team1: 'TEAM7', score1: 1, team2: 'TEAM1', score2: 1 },
+  ]
+
   return (
     <div className="flex min-h-[100dvh] flex-row px-0">
       <main className="flex-1">
         <Header />
         <div className="min-h-screen grid grid-cols-1 md:grid-cols-4">
-          <div className="col-span-1 border border-white"></div>
-          <div className="col-span-2 border border-white">
-            <Card />
+          <div className="col-span-1"></div>
+          <div className="col-span-2">
+            {lists.map((game) => (
+              <Card key={game.id} game={game} />
+            ))}
           </div>
-          <div className="col-span-1 border border-white"></div>
+          <div className="col-span-1"></div>
         </div>
       </main>
     </div>
--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,53 +1,27 @@
 export default function Card({ game }) {
-  //   const isLive = liveGameStatuses.includes(game.status)
   return (
-    <div className="mx-4 mb-2 rounded-[8px] bg-card p-4">
+    <div className="m-4 mb-2 rounded-[8px] bg-card p-4">
       <div className="flex items-center justify-between">
-        {/* <Image
-          src={game.teams.home.logo ?? '/na.webp'}
-          width={24}
-          height={24}
-          className="max-h-[24px] max-w-[24px] object-contain"
-          alt={game.teams.home.name}
-        /> */}
-        <p className="ml-2 truncate font-[450] md:flex-1">STRING</p>
+        <p className="ml-2 truncate font-[450] md:flex-1">{game.team1}</p>
         <div className="flex items-center">
-          <div className="mx-4 rounded bg-primary px-[10px] py-1">NUMBER</div>
-          {/* <PredictButton game={game} predictedWinner={Winner.Home} /> */}
+          <div className="mx-4 rounded bg-primary px-[10px] py-1">
+            {game.score1}
+          </div>
+          <button>Bet</button>
         </div>
       </div>
       <div className="my-3 flex items-center">
-        <span className="mx-3 text-[8px] text-secondary-foreground">@</span>
         <div className="w-[calc(100%-189px)] border-b border-b-primary" />
       </div>
       <div className="flex items-center justify-between">
-        {/* <Image
-          src={game.teams.away.logo ?? '/na.webp'}
-          width={24}
-          height={24}
-          className="max-h-[24px] max-w-[24px] object-contain"
-          alt={game.teams.away.name}
-        /> */}
-        <p className="ml-2 truncate font-[450] md:flex-1">STRING</p>
+        <p className="ml-2 truncate font-[450] md:flex-1">{game.team2}</p>
         <div className="flex items-center">
-          <div className="mx-4 rounded bg-primary px-[10px] py-1">NUMBER</div>
-          {/* <PredictButton game={game} predictedWinner={Winner.Away} /> */}
+          <div className="mx-4 rounded bg-primary px-[10px] py-1">
+            {game.score2}
+          </div>
+          <button>Bet</button>
         </div>
       </div>
-      {/* {isLive && (
-        <Image
-          src="/live.svg"
-          className="pt-1"
-          alt="Live game"
-          width={32}
-          height={16}
-        />
-      )}
-      {!isLive && (
-        <p className="mt-1 text-[12px] font-[450] leading-4 text-secondary-foreground">
-          {`${format(game.date, 'eeee, MMMM d @ h:mm aaa z')}`}
-        </p>
-      )} */}
     </div>
   )
 }
